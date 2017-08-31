c86bf8d12e799602553c863bde4113945f669608
hallgrimur1471 <hallgrimur1471@svarmi.com>
2017-08-31 18:02:47 +0000

    Chapter 1

--- ex1-16-print-all-line-lengths.c
+++ ex1-16-print-all-line-lengths.c
@@ -1,9 +1,8 @@
-/* Revise the main routine of the longest-line program so it will correctly
- * print the length of arbitrarily long input lines, and as much as
- * possible of the text */
+// asdf
+
 
 #include <stdio.h>
-#define MAXLINE 68 /* maximum input line size */
+#define MAXLINE 40 /* maximum input line size */
 
 int getline(char line[], int maxline);
 void copy(char to[], char from[]);
@@ -15,34 +14,27 @@ main()
   int max; /* maximum length seen so far */
   char line[MAXLINE]; /* current input line */
   char longest[MAXLINE]; /* longest line saved here */
+  char tmp[MAXLINE]; /* pass this to getline when line and longest should not be overwritten */
 
   max = 0;
-  while ((len = getline(len, MAXLINE)) > 0) {
+  while ((len = getline(line, MAXLINE)) > 0) {
+    while (len==MAXLINE) /* we didn't get the whole line */
+      len += getline(tmp, MAXLINE);
     if (len > max) {
       max = len;
       copy(longest, line);
     }
+    printf("%d\n", len);
   }
   if (max > 0) /* there was a line */
     printf("%s", longest);
   return 0;
 }
 
-/* getline: read a line into s, return leeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeength */
-int getline(char [s], int lim)
+/* getline: read a line into s, return leeeeeeeeeeee            eeeeeeeeeeeeeength */
+int getline(char s[], int lim)
 {
   int c, i;
-  
-  for (i=0; (c=getchar())!=EOF && c!='\n'; ++i) {
-    if (i < lim-1) {
-      s[i] = c;
-    }
-    else if ((i == lim-1) && (c == '\n')) {
-      s[i] = '\n';
-    }
-    else if (i == lim) {
-      s[i] = '\0';
-  }
 
   for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
     s[i] = c;
