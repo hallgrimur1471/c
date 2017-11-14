535a586ce7e8a249c3379960926d3d8a040474a1
hallgrimur1471 <hallgrimur@svarmi.com>
2017-11-14 09:17:11 +0000

    Chapter 1

--- ex1-10-cp-io-unambiguous-escapes.c
+++ ex1-10-cp-io-unambiguous-escapes.c
@@ -2,6 +2,8 @@
  * tab by \t, each backspace by \b, and each backslash by \\. This makes tabs
  * and backspaces visible in an unambiguous way. */
 
+/* Extra: I also replace new-lines by \n */
+
 #include <stdio.h>
 
 main()
@@ -16,6 +18,10 @@ main()
     else if (c == '\b') {
       putchar('\\');
       putchar('b');
+    }
+    else if (c == '\n') {
+      putchar('\\');
+      putchar('n');
     }
     else if (c == '\\') {
       putchar('\\');
